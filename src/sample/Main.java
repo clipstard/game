f5a8c133fc8eb07004cb600cd745926c429c34ed
Eugeniu <clipstard@mail.ru>
2018-12-06 11:05:10 +0200

    {UPD] new car; camera improved

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -27,18 +27,17 @@ public class Main extends Application {
 
     private static final double W = 1000, H = 640;
 
-    private static final String HERO_IMAGE_LOC = "images/car.png";
-    private static final String MAP_IMAGE_LOC ="images/map.jpg";
+    private static final String HERO_IMAGE_LOC = "images/car2.0.png";
+    private static final String MAP_IMAGE_LOC = "images/map.jpg";
 
 
-
-    private Image heroImage, mapImage ;
-    private Node  hero;
+    private Image heroImage, mapImage;
+    private Node hero;
     private ImageView map, cameraView;
     private double rotateFactor;
     boolean running, goNorth, goSouth, goEast, goWest;
     WritableImage camera;
-private JProgressBar statusBar;
+    private JProgressBar statusBar;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -49,10 +48,10 @@ private JProgressBar statusBar;
         camera = new WritableImage(200, 50);
         cameraView = new ImageView(camera);
         cameraView.setX(0);
-        cameraView.setY( H - camera.getHeight());
+        cameraView.setY(H - camera.getHeight());
 
-        ((ImageView) hero).setFitHeight(100);
-        ((ImageView) hero).setFitWidth(50);
+       /* ((ImageView) hero).setFitHeight(100);
+        ((ImageView) hero).setFitWidth(50);*/
         Group dungeon = new Group();
         dungeon.getChildren().add(map);
         dungeon.getChildren().add(hero);
@@ -65,11 +64,25 @@ private JProgressBar statusBar;
             @Override
             public void handle(KeyEvent event) {
                 switch (event.getCode()) {
-                    case W:    goNorth = true; break;
-                    case S:  goSouth = true; break;
-                    case A:  goWest  = true; break;
-                    case D: goEast  = true; break;
-                    case SHIFT: running = true; break;
+                    case W:
+                    case UP:
+                        goNorth = true;
+                        break;
+                    case S:
+                    case DOWN:
+                        goSouth = true;
+                        break;
+                    case A:
+                    case LEFT:
+                        goWest = true;
+                        break;
+                    case D:
+                    case RIGHT:
+                        goEast = true;
+                        break;
+                    case SHIFT:
+                        running = true;
+                        break;
                 }
             }
         });
@@ -78,11 +91,25 @@ private JProgressBar statusBar;
             @Override
             public void handle(KeyEvent event) {
                 switch (event.getCode()) {
-                    case W:    goNorth = false; break;
-                    case S:  goSouth = false; break;
-                    case A:  goWest  = false; break;
-                    case D: goEast  = false; break;
-                    case SHIFT: running = false; break;
+                    case W:
+                    case UP:
+                        goNorth = false;
+                        break;
+                    case S:
+                    case DOWN:
+                        goSouth = false;
+                        break;
+                    case A:
+                    case LEFT:
+                        goWest = false;
+                        break;
+                    case D:
+                    case RIGHT:
+                        goEast = false;
+                        break;
+                    case SHIFT:
+                        running = false;
+                        break;
                 }
             }
         });
@@ -93,17 +120,19 @@ private JProgressBar statusBar;
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                int  d = 0;
-                double dx =0, dy=0;
+                int d = 0;
+                double dx = 0, dy = 0;
                 if (goNorth) d -= 1;
                 if (goSouth) d += 1;
-                if (goEast)  hero.setRotate(rotateFactor++);
-                if (goWest)  hero.setRotate(rotateFactor--);
-                if (running) { d *= 3; }
+                if (goEast) hero.setRotate(rotateFactor++);
+                if (goWest) hero.setRotate(rotateFactor--);
+                if (running) {
+                    d *= 3;
+                }
 
 
-                dx = ( Math.sin(hero.getRotate()*Math.PI/180) * d);
-                dy =  -(Math.cos(hero.getRotate()*Math.PI/180) * d);
+                dx = (Math.sin(hero.getRotate() * Math.PI / 180) * d);
+                dy = -(Math.cos(hero.getRotate() * Math.PI / 180) * d);
                 drawCamera();
                 moveHeroBy(dx, dy);
             }
@@ -114,9 +143,8 @@ private JProgressBar statusBar;
     private void moveHeroBy(double dx, double dy) {
         if (dx == 0 && dy == 0) return;
 
-        final double cx = hero.getBoundsInLocal().getWidth()  / 2;
+        final double cx = hero.getBoundsInLocal().getWidth() / 2;
         final double cy = hero.getBoundsInLocal().getHeight() / 2;
-        //hero.setRotate(rotateFactor++);
         double x = cx + hero.getLayoutX() + dx;
         double y = cy + hero.getLayoutY() + dy;
 
@@ -124,7 +152,7 @@ private JProgressBar statusBar;
     }
 
     private void moveHeroTo(double x, double y) {
-        final double cx = hero.getBoundsInLocal().getWidth()  / 2;
+        final double cx = hero.getBoundsInLocal().getWidth() / 2;
         final double cy = hero.getBoundsInLocal().getHeight() / 2;
 
         if (x - cx >= 0 &&
@@ -135,21 +163,24 @@ private JProgressBar statusBar;
         }
     }
 
-    private void drawCamera(){
-        double x = hero.getLayoutX();
-        double y = hero.getLayoutY();
+    private void drawCamera() {
+
 
         PixelReader pixelReader = mapImage.getPixelReader();
 
+        double x = (hero.getLayoutX() + (hero.getBoundsInLocal().getWidth() *  Math.sin( - hero.getRotate() * Math.PI / 180)));
+        double y = (hero.getLayoutY() + (hero.getBoundsInLocal().getHeight() *  Math.cos( - hero.getRotate() * Math.PI / 180)));
         //getting the pixel writer
         PixelWriter writer = camera.getPixelWriter();
-System.out.println(hero.getRotate());
-        for(int i = 0; i < camera.getWidth(); i++) {
-            for(int j = 0; j < camera.getHeight(); j++) {
+        for (int i = 0; i < camera.getWidth(); i++) {
+            for (int j = 0; j < camera.getHeight(); j++) {
+                if (hero.getRotate() >= 360) hero.setRotate(0);
+                double px = x - (Math.cos(hero.getRotate() * Math.PI / 180.0) * (x / 20) - Math.sin(hero.getRotate() * Math.PI / 180.0) * (y / 20));
+                double py = y + (Math.cos(hero.getRotate() * Math.PI / 180.0) * (y / 20) + Math.sin(hero.getRotate() * Math.PI / 180.0) * (x / 20));
 
-                double px = x - ( Math.cos(hero.getRotate()*Math.PI/180) * (x/20) - Math.sin(-hero.getRotate()*Math.PI/180) * (y/20));
-                double py = y + ( Math.cos(hero.getRotate()*Math.PI/180) * (y/20) + Math.sin(-hero.getRotate()*Math.PI/180) * (x/20));
-                Color color = pixelReader.getColor(((int)px) + i, (int)py + j);
+                double pi = i / 2.0 + (Math.cos(hero.getRotate() * Math.PI / 180.0) * i - Math.sin(hero.getRotate() * Math.PI / 180.0) * j);
+                double pj = j / 2.0 - (Math.cos(hero.getRotate() * Math.PI / 180.0) * j + Math.sin(hero.getRotate() * Math.PI / 180.0) * i);
+                Color color = pixelReader.getColor((int) (px + pi), (int) (py + pj));
 
                 //Setting the color to the writable image
                 writer.setColor(i, j, color);
@@ -158,5 +189,7 @@ System.out.println(hero.getRotate());
         cameraView = new ImageView(camera);
     }
 
-    public static void main(String[] args) { launch(args); }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
