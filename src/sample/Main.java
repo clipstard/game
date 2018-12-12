66841bc16ff8bcb2a631d59ec64648523383f023
Eugeniu <clipstard@mail.ru>
2018-12-13 00:42:32 +0200

    added import files buttons

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,12 +7,18 @@ import javafx.scene.*;
 import javafx.scene.image.*;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 import org.omg.CORBA.BAD_INV_ORDER;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileInputStream;
 
@@ -30,14 +36,14 @@ public class Main extends Application {
 
 
     private Image heroImage, mapImage, borderImage;
-    private Node hero;
-    private ImageView map, cameraView, border;
+    private ImageView hero, map, cameraView, border;
     private double rotateFactor;
     boolean running, goNorth, goSouth, goEast, goWest;
     WritableImage camera;
     private JProgressBar statusBar;
     private File file;
     private FileInputStream fis;
+    private Button newCarButton, newMapButton;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -49,19 +55,32 @@ public class Main extends Application {
         hero = new ImageView(heroImage);
         camera = new WritableImage(50, 5);
         cameraView = new ImageView(camera);
+        newCarButton = new Button("Select new Car");
+        newMapButton = new Button("Select new map");
         file = new File("C:/Users/Eugeniu/IdeaProjects/untitled2/src/files/input.in");
         fis = new FileInputStream(file);
         border.setScaleY(2);
         border.setX(0);
-        border.setY(H-camera.getHeight() - 4);
+        border.setY(H - camera.getHeight() - 4);
         cameraView.setX(5);
         cameraView.setY(H - camera.getHeight() - 5);
+        newCarButton.setTranslateX((int) camera.getWidth() + 10);
+        newCarButton.setTranslateY((int) H - 35);
+        newCarButton.setPrefSize(200, 20);
+        newMapButton.setTranslateX((int) camera.getWidth() + 220);
+        newMapButton.setTranslateY((int) H - 35);
+        newMapButton.setPrefSize(200, 20);
 
        /* ((ImageView) hero).setFitHeight(100);
         ((ImageView) hero).setFitWidth(50);*/
+        StackPane panel = new StackPane();
+        panel.getChildren().add(newCarButton);
+        panel.getChildren().add(newMapButton);
         Group dungeon = new Group();
+
         dungeon.getChildren().add(map);
         dungeon.getChildren().add(hero);
+        dungeon.getChildren().add(panel);
         dungeon.getChildren().add(border);
         dungeon.getChildren().add(cameraView);
 
@@ -69,6 +88,90 @@ public class Main extends Application {
         rotateFactor = 0;
         Scene scene = new Scene(dungeon, W, H);
 
+        newCarButton.addEventHandler(MouseEvent.MOUSE_CLICKED,
+                new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent e) {
+
+                        JFileChooser file = new JFileChooser();
+                        file.setCurrentDirectory(new File(System.getProperty("user.home")));
+                        //filter the files
+                        FileNameExtensionFilter filter = new FileNameExtensionFilter("*.Images", "jpg", "gif", "png");
+                        file.addChoosableFileFilter(filter);
+                        int result = file.showSaveDialog(null);
+                        //if the user click on save in Jfilechooser
+                        if (result == JFileChooser.APPROVE_OPTION) {
+                            File selectedFile = file.getSelectedFile();
+                            String path = selectedFile.getAbsolutePath();
+                            //ImageIcon temp = new ImageIcon(path);
+                            Point currentLocation = new Point((int) hero.getLayoutX(), (int) hero.getLayoutY());
+                            double currentRotation = hero.getRotate();
+                            dungeon.getChildren().remove(hero);
+                            heroImage = new Image("file:" + path);
+                            hero = new ImageView(heroImage);
+                            hero.setLayoutX(currentLocation.x);
+                            hero.setLayoutY(currentLocation.y);
+                            hero.setRotate(currentRotation);
+                            hero.setFitHeight(100);
+                            hero.setFitWidth(100);
+                            dungeon.getChildren().add(hero);
+
+                        }
+                        //if the user click on save in Jfilechooser
+
+
+                        else if (result == JFileChooser.CANCEL_OPTION) {
+                            System.out.println("No File Select");
+                        }
+                    }
+                });
+
+
+        newMapButton.addEventHandler(MouseEvent.MOUSE_CLICKED,
+                new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent e) {
+
+                        JFileChooser file = new JFileChooser();
+                        file.setCurrentDirectory(new File(System.getProperty("user.home")));
+                        //filter the files
+                        FileNameExtensionFilter filter = new FileNameExtensionFilter("*.Images", "jpg", "gif", "png");
+                        file.addChoosableFileFilter(filter);
+                        int result = file.showSaveDialog(null);
+                        //if the user click on save in Jfilechooser
+                        if (result == JFileChooser.APPROVE_OPTION) {
+                            File selectedFile = file.getSelectedFile();
+                            String path = selectedFile.getAbsolutePath();
+
+                            Image auxMapImage = new Image("file:" + path);
+                            ImageView auxMap = new ImageView(auxMapImage);
+                            if (auxMap.getFitWidth()>= H || auxMap.getBoundsInLocal().getWidth() > 600) {
+                                dungeon.getChildren().remove(map);
+                                dungeon.getChildren().remove(hero);
+                                dungeon.getChildren().remove(panel);
+                                dungeon.getChildren().remove(border);
+                                dungeon.getChildren().remove(cameraView);
+                                mapImage = new Image("file:" + path);
+                                map = new ImageView(mapImage);
+                                map.setFitHeight(H);
+                                map.setFitWidth(W);
+                                dungeon.getChildren().add(map);
+                                dungeon.getChildren().add(hero);
+                                dungeon.getChildren().add(panel);
+                                dungeon.getChildren().add(border);
+                                dungeon.getChildren().add(cameraView);
+                                cameraView.setX(5);
+                                cameraView.setY(H - camera.getHeight() - 5);
+                            } else {
+                                System.out.println("Image size not supported");
+                            }
+                        }
+                        //if the user click on save in Jfilechooser
+                        else if (result == JFileChooser.CANCEL_OPTION) {
+                            System.out.println("No File Select");
+                        }
+                    }
+                });
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
@@ -211,10 +314,10 @@ public class Main extends Application {
                 double px = x - (Math.cos(hero.getRotate() * Math.PI / 180.0) * (x / 20) - Math.sin(hero.getRotate() * Math.PI / 180.0) * (y / 20));
                 double py = y + (Math.cos(hero.getRotate() * Math.PI / 180.0) * (y / 20) + Math.sin(hero.getRotate() * Math.PI / 180.0) * (x / 20));
 
-                double pi = i / 10.0 + (Math.cos(hero.getRotate() * Math.PI / 180.0) * i/10 - Math.sin(hero.getRotate() * Math.PI / 180.0) * j/10);
-                double pj = j / 10.0 - (Math.cos(hero.getRotate() * Math.PI / 180.0) * j/10 + Math.sin(hero.getRotate() * Math.PI / 180.0) * i/10);
-                if ( px + pi < W && py+ pj < H && px+pi > 0 && py+pj > 0){
-                   color = pixelReader.getColor(Math.abs((int) (px + pi)), Math.abs((int) (py + pj)));
+                double pi = i / 10.0 + (Math.cos(hero.getRotate() * Math.PI / 180.0) * i / 10 - Math.sin(hero.getRotate() * Math.PI / 180.0) * j / 10);
+                double pj = j / 10.0 - (Math.cos(hero.getRotate() * Math.PI / 180.0) * j / 10 + Math.sin(hero.getRotate() * Math.PI / 180.0) * i / 10);
+                if (px + pi < map.getImage().getWidth() && py + pj < map.getImage().getHeight() && px + pi > 0 && py + pj > 0) {
+                    color = pixelReader.getColor(Math.abs((int) (px + pi)), Math.abs((int) (py + pj)));
                 }
 
 
@@ -226,46 +329,49 @@ public class Main extends Application {
     }
 
     public Point getNextMove() throws Exception {
-            int x = 0, y = 0;
-            if (fis.available() <= 0) {
-                fis = new FileInputStream(file);
-            }
-            x = fis.read();
-            y = fis.read();
-            if ( x != 45 || x != 32 || x!= 48 || x!= 49) {
-                x = 0;
-            }
-        if ( y != 45 || y != 32 || y!= 48 || y!= 49) {
+        int x = 0, y = 0;
+        if (fis.available() <= 0) {
+            fis = new FileInputStream(file);
+        }
+        x = fis.read();
+        y = fis.read();
+        if (x != 45 || x != 32 || x != 48 || x != 49) {
+            x = 0;
+        }
+        if (y != 45 || y != 32 || y != 48 || y != 49) {
             y = 0;
         }
-            if ( x == 32) {
-                while (x == 32) {
-                    x = fis.read();
-                }
+        if (x == 32) {
+            while (x == 32) {
+                x = fis.read();
             }
-            if ( y == 32){
-                while(y ==32){
-                    y = fis.read();
-                }
-            };
-
-            if (x == 48) x = 0;
-            if ( y == 48) y = 0;
-            if (x == 49) x = 1;
-            if(y == 49) y = 1;
-            if (x==45) {
-                x= fis.read();
-                x = -1;
-            }
-            if(y==45){
+        }
+        if (y == 32) {
+            while (y == 32) {
                 y = fis.read();
-                y = -1;
             }
-            return new Point(x, y);
+        }
+        ;
+
+        if (x == 48) x = 0;
+        if (y == 48) y = 0;
+        if (x == 49) x = 1;
+        if (y == 49) y = 1;
+        if (x == 45) {
+            x = fis.read();
+            x = -1;
+        }
+        if (y == 45) {
+            y = fis.read();
+            y = -1;
+        }
+        return new Point(x, y);
 
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
 }
